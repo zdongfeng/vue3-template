678ae997fe1fc29d505b65e5a942aa7dc9bfe217
dongfang <1790956510@qq.com>
2022-09-27 16:40:49 +0800

    feat: 初始化

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+/*
+ * @Descripttion: 
+ * @Author: zhaodongfeng
+ * @Date: 2022-09-27 15:45:16
+ * @LastEditors: zhaodongfeng
+ * @LastEditTime: 2022-09-27 15:58:51
+ */
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential",
+        "plugin:@typescript-eslint/recommended",
+        'plugin:prettier/recommended' 
+    ],
+    "overrides": [
+    ],
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "@typescript-eslint/ban-types": [
+            "error",
+            {
+              "extendDefaults": true,
+              "types": {
+                "{}": false
+              }
+            }
+        ],
+        "@typescript-eslint/no-explicit-any": ["off"]
+    }
+}
